1f1420b141bea39d3e778b7cafdd797a30fff8c0
smarterwater <49963298+smarterwater@users.noreply.github.com>
2019-04-30 15:46:59 -0500

    Update ledSend.ino

--- ledSend.ino
+++ ledSend.ino
@@ -1,6 +1,7 @@
 // code snippet used from https://forum.arduino.cc/index.php?topic=433725.0
 #include "FastLED.h"
 #include <LiquidCrystal.h>
+#include "Wire.h"
 
 #define NUM_LEDS 300 // Number of LEDS in the strip
 #define DATA_PIN 6
@@ -9,43 +10,38 @@
 CRGB leds[NUM_LEDS];
 
 const int sensorPin = A0;
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; //LCD Pins initialized
-int count = 0;
 unsigned long lastUpdate;
 int sensorValue;
-char valueChar[5];
-char mystr[5] = "Hello";
-String str;
+char str[4];
+int x; // stores the byte from i2c connection
 
-
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   Serial.begin(9600);
+  Wire.begin(9);
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   lcd.begin(16,2);
   pinMode(sensorPin, INPUT);
+  Wire.onReceive(receiveEvent);
+}
+
+void receiveEvent(int bytes) {
+  x = Wire.read();
 }
 
 void loop() {
-  sensorValue = analogRead(sensorPin);
-  if (sensorValue == 0) return;
-
-  //Serial.write(mystr,5); 
-  //delay(1000);
-  str = String(sensorValue);
-  //Serial.write(str);
-  
-  str.toCharArray(valueChar, 5);  
-  Serial.write(valueChar);
-  //Serial.write(sensorValue);
-  
-  
-  Serial.print("\n");
-  lightUp(sensorValue);
-  
-  
-  displayLCD(sensorValue);
+  if (x == 'S') {
+    sensorValue = analogRead(sensorPin);
+    if (sensorValue == 0) return;
+    itoa(sensorValue, str, 10);
+    Serial.write(str, 4);  
+    
+    Serial.print("\n");
+    lightUp(sensorValue);
+  }
+  else if (x == 'E') {
+  }
 }
 
 void lightUp(int val) {
@@ -64,6 +60,7 @@ void lightUp(int val) {
       leds[0] = CRGB:: Red;
       leds[1] = CRGB:: Red;
       leds[2] = CRGB:: Red;
+      
     }
     else if(numLedsToLight >= 50) {
       leds[0] = CRGB:: Green;
@@ -80,27 +77,3 @@ void lightUp(int val) {
       FastLED.show();
     }
   }
-
-void displayLCD(int val) {
-
-  lcd.setCursor(0, 0);
-  
-  
-  while(Serial.available() > 0) {
-    
-    if (Serial.read() >= 200) {
-      lcd.print("IT'S LIT");
-    }
-    else if(Serial.read() >= 150) {
-      lcd.print("Kinda lit");
-    }
-    else if(Serial.read() >= 100) {
-      lcd.print("Eh..");
-    }
-    else if(Serial.read() >= 50) {
-      lcd.print("Lame...");
-    }
-    
-  }
-  
-}
